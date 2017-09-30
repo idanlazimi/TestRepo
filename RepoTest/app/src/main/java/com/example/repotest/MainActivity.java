e612af9d6aa7b1e99b0c3f54da631bff69de57ab
idan <idan@moveo.co.il>
2017-09-30 17:31:15 +0300

    Hello Text Init

--- RepoTest/app/src/main/java/com/example/repotest/MainActivity.java
+++ RepoTest/app/src/main/java/com/example/repotest/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.repotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myText = (TextView) findViewById(R.id.main_activity_helloWorld)
+
+        myText.setText("Hello Buddy");
     }
 }
